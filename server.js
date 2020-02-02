1dcee014e89b1d36d8d298a788bcd5d4d69ee663
ahmed amin <ahmed.amin@integrant.com>
2020-02-02 08:44:21 +0200

    implement timestamp service

---
+++ server.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that the API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+
+// your first API endpoint... 
+app.get("/api/timestamp/:date_string?", function (req, res) {
+    var strDate = req.params.date_string;
+    console.log('strDate:'+strDate);
+    var inputDate = new Date();
+    if(strDate != null && strDate != undefined && strDate != ''){
+        var unixTime = Date.parse(strDate);
+        console.log('unixTime: '+unixTime);
+        if(isNaN(unixTime)){ 
+            res.json({"error" : "Invalid Date" });
+            return;
+        }
+        inputDate.setTime(unixTime);
+        
+    }
+    res.json({"unix": inputDate.getTime(), "utc" : inputDate.toUTCString() })
+});
+
+
+
+// listen for requests :)
+var defaultPort = 3000;
+var listener = app.listen(process.env.PORT || defaultPort, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
